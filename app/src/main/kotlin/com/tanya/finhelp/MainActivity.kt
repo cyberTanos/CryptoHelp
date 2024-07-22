dc2b91dfcd01f1a8e960110e60c54a3fe5c6801e
tanya <swishtan@mail.ru>
2024-07-22 10:45:16 +0300

    TASK-1: Тест api

--- app/src/main/kotlin/com/tanya/finhelp/MainActivity.kt
+++ app/src/main/kotlin/com/tanya/finhelp/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.tanya.finhelp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val vm: VM by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        vm.state.observe(this) {
+            findViewById<TextView>(R.id.textView).text = it.toString()
+        }
+        vm.getCoins()
     }
 }
